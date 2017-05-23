c839e427feb70a6ff319232abc697b38e2737521
= <alexininternet@gmail.com>
2017-05-23 14:08:38 +0300

    update composer

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $database = new QueryBuilder(Connection::make(require 'Data/configDB.php'));
 
 $users = $database->selectAll('user');
 
-//var_dump($users);
+var_dump($users);
 
-echo "Username of user with id {$users[0]->id} is {$users[0]->username}" ;
+//echo "Username of user with id {$users[0]->id} is {$users[0]->username}" ;
